ddb3446856d437e8dfa5f637254e3de2f4cd5921
tychang <tychang@Nexify.com.hk>
2019-09-03 11:42:47 +0800

    first commit formally

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './Page/login/login.component';
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
